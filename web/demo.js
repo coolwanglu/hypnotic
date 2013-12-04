5b719027ccda4f7a548e8af1a82e2a4db3b84146
Lu Wang <coolwanglu@gmail.com>
2013-12-05 02:25:41 +0800

    ..

--- web/demo.js
+++ web/demo.js
@@ -1,40 +1,6 @@
 //google analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-46194653-1', 'coolwanglu.github.io'); ga('send', 'pageview');
 
-//github api
-//credits: http://resume.github.io/
-function get_github_user_stars (username) {
-    var repos = [];
-    $.ajax({
-        url: 'https://api.github.com/users/' + username + '/starred',
-        async: false,
-        dataType: 'json',
-        success: function(json) {
-            repos = json;
-        }
-    });
-    return repos;
-}
-
-function is_github_user_starred_hypnotic (username) {
-    return get_github_user_stars(username).some(function(item) {
-        return item.full_name == 'coolwanglu/hypnotic';
-    });
-}
-
-function load_github_user_gists (username) {
-    var gists = [];
-    $.ajax({
-        url: 'https://api.github.com/users/' + username + '/gists',
-        async: false,
-        dataType: 'json',
-        success: function(json) {
-            gists = json;
-        }
-    });
-    return gists;
-}
-
 function load_github_user_gist_raw_file (gist_id, fn) {
     var src = document.getElementById('source');
     var err_msg = '/* Error: could not load gist */';
@@ -64,6 +30,11 @@ $(function() {
     // scroll to the bottom of the source code after the tab is shown
     $('#link-play').on('shown.bs.tab', function() {
         src.scrollTop = src.scrollHeight;
+    });
+
+    document.getElementById('load-semaphore-demo').addEventListener('click', function() {
+        load_github_user_gist_raw_file('7786903', 'Semaphore');
+        $('#output').html('Here\'s the demo!');
     });
 
     document.getElementById('link-try-your-own').addEventListener('click', function() {
