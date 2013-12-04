9a16394a3ce35229c4ac8fb8f10630a24704e32e
Lu Wang <coolwanglu@gmail.com>
2013-12-04 14:47:20 +0800

    working on demo

--- web/demo.js
+++ web/demo.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var btn = document.getElementById('execute');
     btn.addEventListener('click', function() {
         btn.disabled = true;
+        btn.innerHTML = 'Running...';
         Hypnotic.execute(src.value, function(){
             btn.disabled = false;
+            btn.innerHTML = 'Execute!';
         });
     });
 });
